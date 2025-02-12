70c71768a31479abf903300a4c766bff0db81a9d
hbutt <hbutt@student.s19.be>
2025-02-12 15:58:17 +0100

    check args & struct

--- src/parsing.c
+++ src/parsing.c
@@ -1,36 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbutt <hbutt@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/06 14:15:39 by hbutt             #+#    #+#             */
-/*   Updated: 2025/02/12 14:30:50 by hbutt            ###   ########.fr       */
+/*   Created: 2025/02/10 18:03:39 by hbutt             #+#    #+#             */
+/*   Updated: 2025/02/10 18:03:40 by hbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-# define CUB3D_H
-
-# include "libft.h"
-# include <stdio.h>
-# include <string.h>
-
-typedef struct s_mapinfo
-{
-	int		width;
-	int		height;
-	char	*north;
-	char	*south;
-	char	*east;
-	char	*west;
-	int		pos_x;
-	int		pos_y;
-    
-}			t_mapinfo;
-
-/* CHECK ARGS */
-int			check_args(int ac, char **av);
-
-#endif
