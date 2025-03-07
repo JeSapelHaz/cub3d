cd89f144f4b3dc2ce012ca1d83c8cbf72daf59f6
hbutt <hbutt@student.s19.be>
2025-03-07 16:00:47 +0100

    norme

--- src/ray_casting.c
+++ src/ray_casting.c
@@ -1,32 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ray_casting.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbutt <hbutt@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/06 14:14:31 by hbutt             #+#    #+#             */
-/*   Updated: 2025/03/07 15:54:30 by hbutt            ###   ########.fr       */
+/*   Created: 2025/03/07 15:52:59 by hbutt             #+#    #+#             */
+/*   Updated: 2025/03/07 15:54:44 by hbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
-
-int	main(int ac, char **av)
-{
-	t_data	data;
-
-	if (check_args(ac, av) != 0)
-		return (1);
-	init_data(&data);
-	parse_data(av[1], &data);
-	if (take_info_file(data.mapinfo.file, &data))
-		return (1);
-	if (check_data(data))
-		return (1);
-	init_mlx(&data);
-	mlx_key_hook(data.window, controls, &data);
-	mlx_hook(data.window, 17, 0, (void *)exit, 0);
-	mlx_loop(data.mlx);
-	free_data(&data);
-}
