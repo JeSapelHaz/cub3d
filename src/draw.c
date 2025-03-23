6883e4bd71fe309b880becb5377e855fed616b1d
OctopusSW <hugo.dlbcq@gmail.com>
2025-03-23 17:53:11 +0100

    add a quest in todo, fix direction again

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: hdelbecq <hdelbecq@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 15:02:44 by hbutt             #+#    #+#             */
-/*   Updated: 2025/03/21 16:27:00 by hdelbecq         ###   ########.fr       */
+/*   Updated: 2025/03/23 17:43:19 by hdelbecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,39 +49,39 @@ void	clear_image(t_data *data)
 		}
 	}
 }
+// draw mini map
+void	draw_map(t_data *data)
+{
+	int	x_map;
+	int	y_map;
+	int	x_screen;
+	int	y_screen;
+	int	i;
+	int	j;
+	int	color;
 
-// void	draw_map(t_data *data)
-// {
-// 	int	x_map;
-// 	int	y_map;
-// 	int	x_screen;
-// 	int	y_screen;
-// 	int	i;
-// 	int	j;
-// 	int	color;
-
-// 	y_map = -1;
-// 	while (++y_map < data->mapinfo.map_height)
-// 	{
-// 		x_map = -1;
-// 		while (++x_map < (int)ft_strlen(data->mapinfo.map[y_map]))
-// 		{
-// 			if (data->mapinfo.map[y_map][x_map] == '1')
-// 				color = WHITE;
-// 			else
-// 				color = BLACK;
-// 			y_screen = y_map * TILE_SIZE;
-// 			x_screen = x_map * TILE_SIZE;
-// 			i = -1;
-// 			while (++i < TILE_SIZE)
-// 			{
-// 				j = -1;
-// 				while (++j < TILE_SIZE)
-// 					put_pixel_to_image(data, x_screen + i, y_screen + j, color);
-// 			}
-// 		}
-// 	}
-// }
+	y_map = -1;
+	while (++y_map < data->mapinfo.map_height)
+	{
+		x_map = -1;
+		while (++x_map < (int)ft_strlen(data->mapinfo.map[y_map]))
+		{
+			if (data->mapinfo.map[y_map][x_map] == '1')
+				color = WHITE;
+			else
+				color = BLACK;
+			y_screen = y_map * TILE_SIZE;
+			x_screen = x_map * TILE_SIZE;
+			i = -1;
+			while (++i < TILE_SIZE)
+			{
+				j = -1;
+				while (++j < TILE_SIZE)
+					put_pixel_to_image(data, x_screen + i, y_screen + j, color);
+			}
+		}
+	}
+}
 
 bool	hit_wall(float px, float py, t_data *data)
 {
