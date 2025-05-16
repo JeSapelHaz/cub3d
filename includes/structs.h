595d816fdd190c429bc4410e9652b6446df2171d
OctopusSW <hugo.dlbcq@gmail.com>
2025-05-16 16:09:35 +0200

    norm color.h and struct.h

--- includes/structs.h
+++ includes/structs.h
@@ -6,7 +6,7 @@
 /*   By: hdelbecq <hdelbecq@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 15:19:39 by hbutt             #+#    #+#             */
-/*   Updated: 2025/05/16 15:08:52 by hdelbecq         ###   ########.fr       */
+/*   Updated: 2025/05/16 16:09:10 by hdelbecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ typedef struct s_data	t_data;
 
 typedef struct s_texture
 {
-	char *path; //
+	char				*path;
 	void				*img;
 	char				*img_addr;
 	int					bpp;
@@ -27,13 +27,6 @@ typedef struct s_texture
 	int					height;
 }						t_texture;
 
-// enum e_direction {
-//     NORTH = 0,
-//     SOUTH = 1,
-//     EAST = 2,
-//     WEST = 3
-// };
-
 typedef struct s_mapinfo
 {
 	char				**file;
@@ -41,16 +34,7 @@ typedef struct s_mapinfo
 	char				**copy_map;
 	int					map_height;
 	int					map_width;
-	// char				*north_path;
-	// char				*south_path;
-	// char				*east_path;
-	// char				*west_path;
-	// char 				**paths_textures; //
 	t_texture			textures[4];
-	// t_texture			texture_north;
-	// t_texture			texture_south;
-	// t_texture			texture_east;
-	// t_texture			texture_west;
 	char				*floor;
 	char				*ceiling;
 	int					ceiling_color;
