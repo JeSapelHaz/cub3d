1e663a78d643d5e478f0b2a41c207341e261b7bc
OctopusSW <hugo.dlbcq@gmail.com>
2025-05-18 01:14:19 +0200

    mini map dans fond si pas de mur

--- src/draw_map.c
+++ src/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: hdelbecq <hdelbecq@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 19:35:06 by hbutt             #+#    #+#             */
-/*   Updated: 2025/05/18 00:48:15 by hdelbecq         ###   ########.fr       */
+/*   Updated: 2025/05/18 01:12:29 by hdelbecq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ static int	selec_color(t_data *data, int x_map, int y_map)
 		|| x_map >= (int)ft_strlen(data->mapinfo.map[y_map]))
 		return (-1);
 	if (data->mapinfo.map[y_map][x_map] == '1')
-		color = WHITE;
+		color = MAP_WALL;
 	else if (data->mapinfo.map[y_map][x_map] == '0'
 		|| data->mapinfo.map[y_map][x_map] == '2'
 		|| data->mapinfo.map[y_map][x_map] == 'N'
 		|| data->mapinfo.map[y_map][x_map] == 'S'
 		|| data->mapinfo.map[y_map][x_map] == 'E'
 		|| data->mapinfo.map[y_map][x_map] == 'W')
-		color = BLACK;
+		color = MAP_EMPTY;
 	else
 		return (-1);
 	return (color);
@@ -75,7 +75,6 @@ static void	draw_mini_map(t_data *data, int x[2], int y[2])
 	int	i;
 	int	j;
 
-	color = BLACK;
 	y[0]--;
 	j = 0;
 	while (++y[0] < y[1])
@@ -84,13 +83,14 @@ static void	draw_mini_map(t_data *data, int x[2], int y[2])
 		i = 0;
 		while (++x_tmp < x[1])
 		{
-			if (!draw_player(data, x_tmp, y[0]))
+			if (draw_player(data, x_tmp, y[0]))
+				put_pixel_to_image(data, i, j, RED);
+			else
 			{
 				color = selec_color(data, x_tmp / TILE_SIZE, y[0] / TILE_SIZE);
-				put_pixel_to_image(data, i, j, color);
+				if (color != -1)
+					put_pixel_to_image(data, i, j, color);
 			}
-			else
-				put_pixel_to_image(data, i, j, RED);
 			i++;
 		}
 		j++;
